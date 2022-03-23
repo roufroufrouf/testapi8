8d7ba00b9023117bbee2454e406c8d780b41c159
Abdurrouf <devrouf@gmail.com>
2022-03-23 11:32:07 +0700

    refactor code using query builder

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -4,38 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Siswa;
+use Illuminate\Support\Facades\DB;
 
 class SiswaController extends Controller
 {
     public function index() {
-        return Siswa::all();
+        return DB::table('siswas')->get();
     }
 
     public function create(request $request) {
-        $siswa = new Siswa;
-        $siswa->nama = $request->nama;
-        $siswa->alamat = $request->alamat;
-        $siswa->save();
-
+        DB::table('siswas')->insert([
+            'nama' => $request->nama,
+            'alamat' => $request->alamat,
+        ]);
         return "Data berhasil masuk";
     }
 
 
     public function update(request $request, $id) {
-        $nama = $request->nama;
-        $alamat = $request->alamat;
-
-        $siswa = Siswa::find($id);
-        $siswa->nama = $nama;
-        $siswa->alamat = $alamat;
-        $siswa->save();
+        DB::table('siswas')
+            ->where('id', '=', $id)
+            ->update([
+                'nama' => $request->nama,
+                'alamat' => $request->alamat,
+            ]);
 
         return "Data berhasil di update";
     }
 
     public function delete($id) {
-        $siswa = Siswa::find($id);
-        $siswa->delete();
+        DB::table('siswas')->where('id', '=', $id)->delete();
 
         return "Data berhasil di hapus";
     }
