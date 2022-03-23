32f295d3184d8937074547cc40ac90cb96d62167
Abdurrouf <devrouf@gmail.com>
2022-03-23 11:05:19 +0700

    tutorial api crud eloquent

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('siswa', 'SiswaController@index');
+// Route::post('siswa', 'SiswaController@create');
+// Route::put('/siswa/{id}', 'SiswaController@update');
+// Route::delete('/siswa/{id}', 'SiswaController@delete');
+Route::get('siswa', [SiswaController::class, 'index']);
+Route::post('siswa', [SiswaController::class, 'create']);
+Route::put('/siswa/{id}', [SiswaController::class, 'update']);
+Route::delete('/siswa/{id}', [SiswaController::class, 'delete']);
